e7a1f8ea642c5e1662095c104cc4d211b89fdd3b
javid shabin <javid.main.act@gmail.com>
2024-09-16 22:12:20 +0530

    Create

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser())
 
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
+
 app.use('/api', apiRouter)
 
 // database connection
